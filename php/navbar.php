407d51658a3dd0669add5ec27d764c6b03d921a1
Donghoon(Jack) <varensio3@gmail.com>
2018-09-07 11:19:38 +1000

    first commit

---
+++ php/navbar.php
@@ -0,0 +1,35 @@
+<!-- Nav Bar -->
+<nav class="navbar navbar-expand-md bg-light">
+  <a class="navbar-brand" href="#">Logo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon">Collapse Icon</span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About Me</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Projects
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Unity</a>
+          <a class="dropdown-item" href="#">Cyber Security</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Algorithms</a>
+          <a class="dropdown-item" href="#">Extra</a>
+          <a class="dropdown-item" href="#">Extra Extra</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
