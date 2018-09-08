f948cdc01a7c775f6a402286a31770b98c307771
Donghoon(Jack) <varensio3@gmail.com>
2018-09-09 01:12:09 +1000

    Working on the landing page

---
+++ php/logobar.php
@@ -0,0 +1,8 @@
+<!-- logo bar -->
+<div class="container-fluid bg-white" style="height: 50px;" id="identify">
+    <div class="row align-items-center justify-content-center">
+        <div class="col col-xs-6 text-center rounded" id="logobox">
+            <a href="#" class="font-2em" id="logotext">HashTheBlog</a>
+        </div>
+    </div>
+</div>
